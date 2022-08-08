dec5b6810050914f9cefee6adcdc63d71d237aee
tlor0026 <tlor0026@gmail.com>
2022-08-08 00:40:36 -0500

    test2

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,4 @@
+
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
@@ -6,12 +7,9 @@ require('dotenv').config();
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: process.env.DB_HOST,
-      Port: process.env.DB_PORT,
+      host: 'localhost',
       dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
+      port: 3306
     });
 
 module.exports = sequelize;
