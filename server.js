849ccd50f2e6124f5f0cdf6b21cd6d6d5f17d4f7
tlor0026 <tlor0026@gmail.com>
2022-08-07 22:05:34 -0500

    trying to get heroku to work

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
@@ -32,7 +31,7 @@ const sess = {
   app.use(express.urlencoded({ extended: false }));
   app.use(express.static(path.join(__dirname, 'public')));
   
-  app.use(require('./config/connection'));
+  app.use(require('./controllers/'));
   
   sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
