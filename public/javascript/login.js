cd328b2e2eef29aa5e0fe3c69a8596fc345d8429
tlor0026 <103943311+tlor0026@users.noreply.github.com>
2022-08-17 15:01:35 -0500

    Update login.js

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -22,32 +22,6 @@ async function loginFormHandler(event) {
     }
   }
   
-  // async function signupFormHandler(event) {
-  //   event.preventDefault();
-  
-  //   const username = document.querySelector('#username-signup').value.trim();
-  //   const password = document.querySelector('#password-signup').value.trim();
-  
-  //   if (username && password) {
-  //     const response = await fetch('/api/users', {
-  //       method: 'post',
-  //       body: JSON.stringify({
-  //         username,
-  //         password
-  //       }),
-  //       headers: { 'Content-Type': 'application/json' }
-  //     });
-  
-  //     if (response.ok) {
-  //       // alert('Signup Now!');
-  //       // document.location.reload();
-  //       document.location.replace('/dashboard/');
-  //     } else {
-  //       alert(response.statusText);
-  //     }
-  //   }
-  // }
-  
   document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
   
   // document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
